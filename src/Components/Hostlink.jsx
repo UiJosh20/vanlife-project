13c4ffb75bbc37d1e3d74410cf7f607ab0e253ec
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-01-31 15:53:57 +0100

    save

--- src/Components/Hostlink.jsx
+++ src/Components/Hostlink.jsx
@@ -8,15 +8,15 @@ const Hostlink = () => {
       title:'Dashboard'
     },
     {
-      path:'/income',
+      path:'/host/income',
       title:'Income'
     },
     {
-      path:'/vans',
+      path:'/host/vans',
       title:'Vans'
     },
     {
-      path:'/reviews',
+      path:'/host/reviews',
       title:'Reviews'
     },
   ]
@@ -24,7 +24,7 @@ const Hostlink = () => {
     <main>
       {
         hostLink.map((link)=>(
-          <NavLink to={link.path} className={({isA})=>()}>
+          <NavLink to={link.path} className={({isActive})=>(isActive?"border-b-2 border-black":"text-gray-500")}>
             {link.title}
           </NavLink>
         ))
